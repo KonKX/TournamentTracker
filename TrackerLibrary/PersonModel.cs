3dedf2f1d05b4701a7fcffed4d11babd10095c81
KonKX <kon.chrysov@gmail.com>
2021-11-28 17:13:05 +0200

    Add project files.

---
+++ TrackerLibrary/PersonModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TrackerLibrary
+{
+    public class PersonModel
+    {
+        public string FirsName { get; set; } = string.Empty;
+        public string LastName { get; set; } = string.Empty;
+        public string Address { get; set; } = string.Empty;
+        public string CellPhoneNumber { get; set; } = string.Empty;
+    }
+}
